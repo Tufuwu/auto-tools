6371a5ecd1b14506ae72a7777962cf15e41f4b35
wu <2450402591@qq.com>
2025-01-31 15:02:37 +0800

    1

--- main .py
+++ main .py
@@ -2,12 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+'''
 # 配置 Chrome
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")  # 防止被检测
 driver = webdriver.Chrome(options=options)
-
+'''
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 # 1. 打开淘宝并手动登录
 driver.get("https://www.taobao.com")
 input("请在浏览器中手动登录后，按 Enter 继续...")
@@ -17,6 +21,7 @@ item_url = "https://item.taobao.com/item.htm?id=商品ID"
 driver.get(item_url)
 
 # 3. 监控抢购时间
+'''
 while True:
     try:
         buy_button = driver.find_element(By.XPATH, "//a[contains(@class, 'buy-now')]")
@@ -31,3 +36,4 @@ while True:
 time.sleep(1)
 submit_button = driver.find_element(By.XPATH, "//button[contains(@class, 'go-btn')]")
 submit_button.click()
+'''
