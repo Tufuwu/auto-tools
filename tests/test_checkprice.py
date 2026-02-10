94638f685430f2a0806f89e336e713792ae4f1f8
wu <wu@.com>
2026-02-10 22:44:09 +0800

    update

--- tests/test_checkprice.py
+++ tests/test_checkprice.py
@@ -2,7 +2,7 @@ from autotools.core.get_message import get_fund_real_time_estimation
 import pandas as pd
 from pathlib import Path
 
-def run(csv_file, base_dir):
+def test(csv_file, base_dir):
     df = pd.read_csv(csv_file, dtype={'number': str})
 
     save_path = Path(base_dir) / 'autotools' / 'resources' / 'message.txt'
@@ -29,4 +29,4 @@ def run(csv_file, base_dir):
 if __name__ == "__main__":
     base_dir = Path(__file__).parent.parent
     csv_file = base_dir/'autotools'/"resources"/'target_csv.csv'
-    run(csv_file,base_dir)
+    test(csv_file,base_dir)
